b8de546bbf5935f54aa02209c5992a35d0af3b48
HadassahAckerman <116038042+HadassahAckerman@users.noreply.github.com>
2023-06-06 14:24:22 +0300

    lesson_06_06

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter';
+import InputVal from './components/inputVal';
+import Message from './components/message';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='text-danger'>React</h1>
+      <Message txt="first 1111"/>
+      <Message txt="last 2222"/>
+      <Counter></Counter>
+      <InputVal/>
+
     </div>
   );
 }
