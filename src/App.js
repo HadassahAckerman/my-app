9fd56db0803d1a517a352586721a06de7a8876ec
HadassahAckerman <116038042+HadassahAckerman@users.noreply.github.com>
2023-06-15 10:47:45 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AppHw from './comps_hw/appHw';
+import AppRoutes from './comps_routes/appRoutes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <React.Fragment>
+   <AppRoutes />
+   {/* <AppHw /> */}
+  </React.Fragment>
+ );
 }
 
 export default App;
